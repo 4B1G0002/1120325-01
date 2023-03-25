4206d2b3954ff5ec932d2cd553629abc5251f634
士釗 馮 <4b1g0002@gmail.com>
2023-03-25 09:56:30 +0800

    加入專案檔案。

---
+++ 1120325-01.cpp
@@ -0,0 +1,22 @@
+﻿// 1120325-01.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+// 指標
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var = 20;
+    int* ip;
+    ip = &var;
+
+    cout << "var = " << var << endl;
+    cout << "$var = " << &var << endl;
+    cout << "ip = " << ip << endl;
+    cout << "*ip = " << *ip << endl;
+
+    int** ipp;
+    ipp = &ip;
+    cout << "ipp=  " << ipp << endl;
+    cout << "*ipp = " << *ipp << endl;
+}
